9dd2e6c39018a48e33cceadf407d619133368734
Leo <leomauv@gmail.com>
2020-05-11 23:46:37 +0200

    Reproduction MissingMethodExeption

---
+++ App1/App1/App1/MainPage.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SkiaSharp;
+using SkiaSharp.Views.Forms;
+using Xamarin.Forms;
+
+namespace App1
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Handle_PaintSurface(object sender, SKPaintSurfaceEventArgs e)
+        {
+            var canvas = e.Surface.Canvas;
+            using (var paint = new SKPaint())
+            {
+                paint.Color = Color.Black.ToSKColor();
+                paint.TextSize = 30;
+
+                canvas.DrawText("I am text", 15, 15, paint);
+            }
+        }
+    }
+}
